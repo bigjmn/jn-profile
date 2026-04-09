979927244ef174513e6271c522181adcab709604
Jesse <bigjmn@gmail.com>
2026-04-08 20:13:02 -0400

    typefix

--- src/components/widgets/WidgetRenderer.tsx
+++ src/components/widgets/WidgetRenderer.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import dynamic from 'next/dynamic';
+import type { ComponentType } from 'react';
 
 import { widgets } from '@/lib/widgets';
 
-const widgetComponents = widgets.reduce<Record<string, ReturnType<typeof dynamic>>>((acc, widget) => {
-  acc[widget.slug] = dynamic(widget.load, { ssr: false });
+const widgetComponents = widgets.reduce<Record<string, ComponentType>>((acc, widget) => {
+  acc[widget.slug] = dynamic(
+    () => widget.load() as Promise<{ default: ComponentType }>,
+    { ssr: false }
+  );
   return acc;
 }, {});
 
